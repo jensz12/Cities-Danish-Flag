7c98f2179946dcdc4a6fe2d378c23fc7d152adbd
Jens Møller <jens@jensz12.com>
2016-09-20 14:19:59 +0200

    Add files via upload

---
+++ source/ShroomblazeFlags.cs
@@ -0,0 +1,25 @@
+﻿using ICities;
+
+namespace DanishFlag
+{
+    public class DanishFlag : IUserMod
+    {
+        public string Name
+        {
+            get { return "Danish flag"; }
+        }
+
+        public string Description
+        {
+            get { return "Danish flag"; }
+        }
+
+        public string[][] CustomFlags()
+        {
+            return new[]
+            {
+                new[] {"denmark", "Denmark"},
+            };
+        }
+    }
+}
